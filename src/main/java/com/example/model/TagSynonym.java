29f3ee8302d9acccef8fd22929bc5b7b6f2d4351
anan0120 <158989903@qq.com>
2019-09-17 14:07:47 +0800

    test

---
+++ src/main/java/com/example/model/TagSynonym.java
@@ -0,0 +1,19 @@
+package com.example.model;
+
+import net.csdn.common.exception.AutoGeneration;
+import net.csdn.jpa.association.Association;
+import net.csdn.jpa.model.Model;
+
+import javax.persistence.OneToMany;
+import java.util.List;
+
+import static net.csdn.common.collections.WowCollections.list;
+
+public class TagSynonym extends Model {
+    @OneToMany
+    private List<Tag> tags = list();
+
+    public Association tags() {
+        throw new AutoGeneration();
+    }
+}
